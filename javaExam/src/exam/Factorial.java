890d6990c0265d5a0e80b2d52cd465f0befdb793
NirajTandel99 <55706541+NirajTandel99@users.noreply.github.com>
2020-02-11 11:05:22 +0530

    Add files via upload

---
+++ javaExam/src/exam/Factorial.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Factorial {
+	public static void main(String[] args) {
+        int n = 5;
+        long f = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            f = f * i;
+        }
+        System.out.println("Factorial of "+n+" is: "+f);
+    }
+
+}
